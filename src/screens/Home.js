0d3868208016855d742cb6849f8a51560772f2d3
Desktop <charlesworld1994@gmail.com>
2022-04-24 16:05:53 -0700

    finished fetch conditional state from yelp api. pickUp doesnt work. A bug

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -13,6 +13,7 @@ const YELP_API_KEY =
 export default function Home() {
   const [restaurantData, setRestaurantData] = useState(localRestaurants);
   const [city, setCity] = useState("Santa Barbara");
+  const [activeTab, setActiveTab] = useState("Delivery");
   const getRestaurantFromYelp = () => {
     const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
 
@@ -23,15 +24,21 @@ export default function Home() {
     };
     return fetch(yelpUrl, apiOptions)
       .then((res) => res.json())
-      .then((json) => setRestaurantData(json.businesses));
+      .then((json) =>
+        setRestaurantData(
+          json.businesses.filter((business) =>
+            business.transactions.includes(activeTab.toLowerCase())
+          )
+        )
+      );
   };
   useEffect(() => {
     getRestaurantFromYelp();
-  }, [city]);
+  }, [city, activeTab]);
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
-        <HeaderTabs />
+        <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab} />
         <Searchbar cityhandler={setCity} />
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
